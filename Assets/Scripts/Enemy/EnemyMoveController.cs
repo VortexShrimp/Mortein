27dd09e48f68b01ac76ad854ddd588954609d071
Callan <vortexshrimp@gmail.com>
2024-04-16 16:24:02 +0200

    Removed uselesss _rigidbody field since its not being used.

--- Assets/Scripts/Enemy/EnemyMoveController.cs
+++ Assets/Scripts/Enemy/EnemyMoveController.cs
@@ -1,17 +1,14 @@
 using UnityEngine;
 
+/// <summary>
+/// Responsible for moving enemies.
+/// Currently just moves them towards the player.
+/// </summary>
 public class EnemyMoveController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
-    private Rigidbody2D _rigidbody;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        _rigidbody = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
+    // Update is called once per frame.
     void FixedUpdate()
     {
        transform.position = Vector2.MoveTowards(transform.position, GameManager.Instance.playerPos, moveSpeed * Time.fixedDeltaTime);
